3166eab35130f18c6c11805c479aa205d1f5eef6
Richard Gagg <r.gagg@outlook.com>
2026-02-05 14:09:55 +1100

    Initial Commit

---
+++ OlympicLogos/OlympicLogosApp.swift
@@ -0,0 +1,17 @@
+//
+//  OlympicLogosApp.swift
+//  OlympicLogos
+//
+//  Created by Richard Gagg on 5/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct OlympicLogosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
